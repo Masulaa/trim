8d75fa4873fcbea331423d6fa7ef7b84ec880221
Luka <lukamasulovic2@gmail.com>
2024-12-11 13:41:39 +0100

    added categories, and made animations for it. Also worked on responsive side of application

--- src/components/HomeScreen/HomeScreen.js
+++ src/components/HomeScreen/HomeScreen.js
@@ -1,12 +1,28 @@
+import React, { useState, useRef, useEffect } from "react";
 import "./HomeScreen.css";
 import menu from "../../images/menu.png";
-import search from "../../images/search.png";
 
 const HomeScreen = () => {
+  const [activeCategory, setActiveCategory] = useState("Kafe");
+  const [indicatorStyle, setIndicatorStyle] = useState({});
+  const buttonsRef = useRef([]);
+
+  const categories = ["Kafe", "Bezalkoholno", "Piva", "Alkoholno"];
+
+  useEffect(() => {
+    const activeButtonIndex = categories.indexOf(activeCategory);
+    const activeButton = buttonsRef.current[activeButtonIndex];
+    if (activeButton) {
+      setIndicatorStyle({
+        left: `${activeButton.offsetLeft + activeButton.offsetWidth / 2 - 5}px`, // Centriraj tačku
+      });
+    }
+  }, [activeCategory, categories]);
+
   return (
     <div className="background">
       <div className="logoup">
-        <img src={menu} alt="logo" className="logo"></img>
+        <img src={menu} alt="logo" className="logo" />
       </div>
       <div className="headline">
         Online Meni
@@ -18,7 +34,22 @@ const HomeScreen = () => {
           type="text"
           className="search-bar"
           placeholder="  Pronađi željeni proizvod"
-        ></input>
+        />
+      </div>
+      <div className="category-selection">
+        {categories.map((category, index) => (
+          <button
+            key={category}
+            ref={(el) => (buttonsRef.current[index] = el)}
+            className={`category-button ${
+              activeCategory === category ? "active" : ""
+            }`}
+            onClick={() => setActiveCategory(category)}
+          >
+            {category}
+          </button>
+        ))}
+        <div className="indicator" style={indicatorStyle}></div>
       </div>
     </div>
   );
