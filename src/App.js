a98e6557be0412e2599092871d8c343a1c614837
Luka <lukamasulovic2@gmail.com>
2024-12-12 12:34:30 +0100

    finished homepage HARD. Needs refactoring, and more media query css styles

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HomeScreen from "./components/HomeScreen/HomeScreen";
+import Cart from "./components/Cart/Cart";
+import Product from "./components/Product/Product";
 import "./style/global.css";
 
 function App() {
@@ -8,6 +10,8 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<HomeScreen />} />
+        <Route path="/biljeske" element={<Cart />} />
+        <Route path="/product/:id" element={<Product />} />
       </Routes>
     </Router>
   );
--- src/components/HomeScreen/HomeScreen.js
+++ src/components/HomeScreen/HomeScreen.js
@@ -1,23 +1,56 @@
-import React, { useState, useRef, useEffect } from "react";
+import React, { useState, useRef, useEffect, useMemo } from "react";
 import "./HomeScreen.css";
+import drinks from "./drinks";
 import menu from "../../images/menu.png";
+import info from "../../images/info.png";
+import biljeske from "../../images/biljeske.png";
+import home from "../../images/home.png";
 
 const HomeScreen = () => {
   const [activeCategory, setActiveCategory] = useState("Kafe");
   const [indicatorStyle, setIndicatorStyle] = useState({});
+  const [hideFooter, setHideFooter] = useState(false);
+  const [isFooterVisible, setIsFooterVisible] = useState(false);
   const buttonsRef = useRef([]);
 
-  const categories = ["Kafe", "Bezalkoholno", "Piva", "Alkoholno"];
+  const categories = useMemo(
+    () => ["Kafe", "Bezalkoholno", "Piva", "Alkoholno"],
+    []
+  );
 
   useEffect(() => {
     const activeButtonIndex = categories.indexOf(activeCategory);
     const activeButton = buttonsRef.current[activeButtonIndex];
     if (activeButton) {
       setIndicatorStyle({
-        left: `${activeButton.offsetLeft + activeButton.offsetWidth / 2 - 5}px`, // Centriraj tačku
+        left: `${activeButton.offsetLeft + activeButton.offsetWidth / 2 - 5}px`,
       });
     }
   }, [activeCategory, categories]);
+
+  const filteredDrinks = drinks.filter(
+    (drink) => drink.category === activeCategory
+  );
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollTop = window.scrollY;
+      const windowHeight = window.innerHeight;
+      const documentHeight = document.documentElement.scrollHeight;
+
+      const atBottom = Math.abs(scrollTop + windowHeight - documentHeight) <= 1;
+      setHideFooter(atBottom);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  const toggleFooter = () => {
+    setIsFooterVisible((prev) => !prev);
+  };
+
+  const isLargeScreen = window.innerWidth >= 1024;
 
   return (
     <div className="background">
@@ -50,6 +83,60 @@ const HomeScreen = () => {
           </button>
         ))}
         <div className="indicator" style={indicatorStyle}></div>
+      </div>
+
+      <div className="products">
+        {filteredDrinks.map((drink) => (
+          <div className="product" key={drink.id}>
+            <div className="img-product-div">
+              <img
+                src={require(`../../images/${drink.image}`)}
+                alt={drink.name}
+                className="img-product"
+              />
+            </div>
+            <div className="product-info">
+              <p className="product-name">{drink.name}</p>
+              <p className="product-description">{drink.description}</p>
+              <div className="price-and-more">
+                <p className="price-tag">
+                  <span id="eurosign">€</span>
+                  {drink.price.toFixed(2)}
+                </p>
+                <button className="info-button">
+                  <img src={info} alt="info" />
+                </button>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+      {isLargeScreen && (
+        <button
+          className={`custom-button ${isFooterVisible ? "active" : ""}`}
+          onClick={toggleFooter}
+          onMouseEnter={(e) => {
+            if (!isFooterVisible) e.currentTarget.classList.add("hover");
+          }}
+          onMouseLeave={(e) => {
+            if (!isFooterVisible) e.currentTarget.classList.remove("hover");
+          }}
+        >
+          <div className="vector-group">
+            <div className="vector"></div>
+            <div className="vector"></div>
+            <div className="vector"></div>
+            <div className="vector"></div>
+          </div>
+        </button>
+      )}
+      <div
+        className={`fixed-footer ${
+          hideFooter || (!isFooterVisible && isLargeScreen) ? "hidden" : ""
+        }`}
+      >
+        <img src={home} alt="home" className="icons-menu" />
+        <img src={biljeske} alt="biljeske" className="icons-menu" />
       </div>
     </div>
   );
